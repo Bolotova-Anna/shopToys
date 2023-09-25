cc6d555413e22ec7f6301eeb5860c04a0eb40b66
Anna Bolotova <anechkamilaha69@gmail.com>
2023-09-26 01:09:54 +0300

    создан класс конструктор

--- toy.java
+++ toy.java
@@ -1,3 +1,25 @@
 public class toy {
-    
+    String name;
+    Integer losRate;
+    Integer id;
+
+    public toy(Integer id, Integer losRate, String name) {
+        this.name = name;
+        this.losRate = losRate;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        String res = String.format("%d,%s,%s", id, losRate, name);
+        return res;
+    }
+
+    public Integer getLosRate() {
+        return losRate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 }
